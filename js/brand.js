bd22226034258bab94c45e422e751be9916e2e51
jey8850 <d92568850@gmail.com>
2021-07-26 19:01:28 +0900

    0726

--- js/brand.js
+++ js/brand.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
         autoScrolling:true,
         lockAnchors:false,
         anchors:['first','second','third','fourth'],
-        navigation:true
+        navigation:true,
+        navigationTooltips:['섹션이름1','섹션이름2','섹션이름3','섹션이름4'],
+        showActiveTooltip:true,
+        sectionsColor:['#ffcccc','#ffffcc','#ffeecc','#ffeeaa'],//섹션배경색
     });
 });
