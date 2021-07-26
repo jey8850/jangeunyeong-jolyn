1f6b1eb9fc9df1ec444aafe3a9f1e8f5dc35226a
jey8850 <d92568850@gmail.com>
2021-07-26 16:44:24 +0900

    0726

--- js/common.js
+++ js/common.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-    $('.bxslider').bxSlider({
-        auto: true, // 자동 슬라이드 
-        autoControls: false, // 시작, 정지버튼 노출 여부 
-        controls: false, // prev/next 버튼 노출 여부 
-        pager: true, //불릿버튼
-    }); 
-});
+// $(document).ready(function(){
+//     $('.bxslider').bxSlider({
+//         auto: true, // 자동 슬라이드 
+//         autoControls: false, // 시작, 정지버튼 노출 여부 
+//         controls: false, // prev/next 버튼 노출 여부 
+//         pager: true, //불릿버튼
+//     }); 
+// });
 
 
 $(".store").mouseenter(function(){
--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         auto: true, // 자동 슬라이드 
         autoControls: false, // 시작, 정지버튼 노출 여부 
         controls: false, // prev/next 버튼 노출 여부 
-        pager: false //불릿버튼
+        pager: true //불릿버튼
     });
     /*
     var windowWidth = $(window).width(),
