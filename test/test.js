270484902c3319dbaeeef4231dfeab0067ed5ae3
jey8850 <d92568850@gmail.com>
2021-08-02 16:06:53 +0900

    0802

---
+++ test/test.js
@@ -0,0 +1,1 @@
+var currentPosition = parseInt($("#sidebox").css("top")); $(window).scroll(function() { var position = $(window).scrollTop(); $("#sidebox").stop().animate({"top":position+currentPosition+"px"},1000); });
